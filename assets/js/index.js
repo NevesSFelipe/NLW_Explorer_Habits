7ff5ebf8e764bad9f71d8a827293d526c6a6b681
Felipe S. Neves <felipeneves.github@gmail.com>
2023-01-21 15:59:21 -0300

    fourth class

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,31 @@
 const form = document.querySelector('#form-habits');
 const nlwSetup = new NLWSetup(form);
 
-const data = {
-    run: ["01-01", "01-02", "01-03"],
-    water: ["01-05", "01-09", "01-10"],
-    food: ["01-04", "01-07", "01-08"],
+const button = document.getElementById('btn_add');
+
+button.addEventListener('click', add);
+form.addEventListener('change', save);
+
+function add()
+{
+    const today = new Date().toLocaleDateString('pt-br').slice(0, -5);
+    const dayExists = nlwSetup.dayExists(today);
+
+    if(dayExists) {
+        alert('Dia já incluso');
+        return;
+    }
+
+    alert('Dia adicionado com sucesso');
+    nlwSetup.addDay(today);
+
 }
 
+function save()
+{
+    localStorage.setItem('NLW@habits', JSON.stringify(nlwSetup.data));
+}
+
+const data = JSON.parse(localStorage.getItem('NLW@habits')) || {};
 nlwSetup.setData(data);
 nlwSetup.load();
